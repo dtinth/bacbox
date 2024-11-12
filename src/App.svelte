b9cc9787ff7e87ac943fd314d276a72fc89b2563
dtinth on MBP M1 <dtinth@spacet.me>
2024-11-12 15:46:21 +0700

    add code generation

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
+  import md5 from "md5";
   import { onMount } from "svelte";
 
   const n = 100;
   let checkboxes = $state<boolean[]>(Array(n));
   let checkedCount = $derived(checkboxes.filter((e) => !!e).length);
   let win = $derived(checkboxes.length == n && checkedCount == n);
+
+  const values = $state(["Svelte!"]);
 
   onMount(() => {
     // Fill half
@@ -27,21 +30,31 @@
   });
 
   $effect(() => {
+    while (values.length > checkedCount) {
+      values.pop();
+    }
+    while (values.length < checkedCount) {
+      values.push(md5(values[values.length - 1]));
+    }
+  });
+
+  $effect(() => {
     if (win) {
-      alert("Your code is ...");
+      alert("Congratulations!");
     }
   });
 </script>
 
 {#if win}
   <h1>You win!</h1>
+  <p>Your code: {String(values[n - 1]).slice(0, 10)}</p>
 {:else}
   <h1>{checkedCount}/{n}</h1>
 {/if}
 
 <div class="container">
   {#each checkboxes as box, idx}
-    <input type="checkbox" bind:checked={checkboxes[idx]} id={idx} />
+    <input type="checkbox" bind:checked={checkboxes[idx]} id={`${idx}`} />
   {/each}
 </div>
 
